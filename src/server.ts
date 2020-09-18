87964767f29d1f0e80665b9b0b0775ee30d12058
Carlos Wimmer <carloswimmer@gmail.com>
2020-09-18 10:33:24 -0300

    Change server port

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-app.listen(8080, () => {
-  console.log('🚀 Server started on port 8080!');
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
 });
