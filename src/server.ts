cf7aea4e6f9f648d7ed6c5d774ef93aaa17d1ef1
Carlos Wimmer <coiote777@gmail.com>
2020-12-15 15:57:06 -0300

    Change service port

--- src/server.ts
+++ src/server.ts
@@ -36,6 +36,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(8080, () => {
-  console.log('🚀 Server started on port 8080!');
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
 });
